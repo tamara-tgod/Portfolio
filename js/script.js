f443196a7c240ff218ab1bf2f8e83f98c4307128
Tamma-alt <84251673+Tamma-alt@users.noreply.github.com>
2025-01-07 06:48:50 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  // Toggle menu visibility on small screens
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+  });
